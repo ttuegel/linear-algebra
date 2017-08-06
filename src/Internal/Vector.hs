385d5a5432ad3d66e64b318a083cb92752fb1e4c
Thomas Tuegel <ttuegel@mailbox.org>
2017-08-06 06:24:05 -0500

    Internal.Matrix: Unify matrix type signatures

--- src/Internal/Vector.hs
+++ src/Internal/Vector.hs
@@ -4,7 +4,7 @@ module Internal.Vector
     , new, thaw, freeze, copy
     , unsafeNew, unsafeFreeze, unsafeThaw
     , read, write, slice
-    , unsafeRead, unsafeWrite, unsafeSlice
+    , unsafeRead, unsafeWrite
     , advanceForeignPtr
     ) where
 
@@ -22,26 +22,37 @@ import Internal.Int
 import Internal.Mut
 
 
-data V (n :: Dim) a
-  = V
-    {-# UNPACK #-} !(N n)
-    {-# UNPACK #-} !(ForeignPtr a)
-    {-# UNPACK #-} !I
+data V (n :: Nat) a
+  = V { vdim :: {-# UNPACK #-} !(N n)
+      , vptr :: {-# UNPACK #-} !(ForeignPtr a)
+      , vinc :: {-# UNPACK #-} !I
+      }
 
-unsafeIndex :: Storable a => V n a -> I -> a
-unsafeIndex (V _ fptr inc) !i =
-  unsafeInlineIO $ withForeignPtr fptr $ \ptr ->
-    peekElemOff ptr (fromIntegral $! i * inc)
+indexIO :: Storable a => V n a -> N n -> IO a
+indexIO v (N i) = unsafeIndexIO v i
+
+unsafeIndexIO :: Storable a => V n a -> I -> IO a
+unsafeIndexIO (V {..}) i =
+  withForeignPtr vptr $ \ptr ->
+  peekElemOff ptr (fromIntegral (i * vinc))
+
+indexPrim :: (PrimMonad m, Storable a) => V n a -> N n -> m a
+indexPrim v (N i) = unsafeIndexPrim v i
 
 unsafeIndexPrim :: (PrimMonad m, Storable a) => V n a -> I -> m a
-unsafeIndexPrim (V _ fptr inc) !i =
-  unsafePrimToPrim $ withForeignPtr fptr $ \ptr ->
-    peekElemOff ptr (fromIntegral $! i * inc)
+unsafeIndexPrim v i = unsafePrimToPrim (unsafeIndexIO v i)
+
+indexM :: (Monad m, Storable a) => V n a -> N n -> m a
+indexM v (N i) = unsafeIndexM v i
 
 unsafeIndexM :: (Monad m, Storable a) => V n a -> I -> m a
-unsafeIndexM (V _ fptr inc) !i =
-  return $ unsafeInlineIO $ withForeignPtr fptr $ \ptr ->
-    peekElemOff ptr (fromIntegral $! i * inc)
+unsafeIndexM v i = (return . unsafeInlineIO) (unsafeIndexIO v i)
+
+index :: Storable a => V n a -> N n -> a
+index v (N i) = unsafeIndex v i
+
+unsafeIndex :: Storable a => V n a -> I -> a
+unsafeIndex v i = unsafeInlineIO (unsafeIndexIO v i)
 
 unsafeNew :: (PrimMonad m, Storable a) => I -> m (Mut (V n) (PrimState m) a)
 unsafeNew n = unsafePrimToPrim $ do
@@ -74,41 +85,32 @@ freeze as@(Mut (V n _ _)) = do
   case bs of
     Mut v -> pure v
 
-unsafeRead :: (PrimMonad m, Storable a) => Mut (V n) (PrimState m) a -> I -> m a
-unsafeRead (Mut (V _ fptr inc)) i =
-  unsafePrimToPrim $ withForeignPtr fptr $ \ptr ->
-    peekElemOff ptr (fromIntegral $! i * inc)
+read :: (PrimMonad m, Storable a) => Mut (V n) (PrimState m) a -> N i -> m a
+read v (N i) = unsafeRead v i
 
-read :: (PrimMonad m, Storable a, i < n) =>
-        Mut (V n) (PrimState m) a -> N i -> m a
-read v@(Mut (V nn _ _)) ni@(N i)
-  | lessThan ni nn = unsafeRead v i
-  | otherwise =
-      error ("read: index `" ++ show ni
-              ++ "' out of bounds `" ++ show nn ++ "'")
+unsafeRead :: (PrimMonad m, Storable a) =>
+              Mut (V n) (PrimState m) a -> I -> m a
+unsafeRead (Mut (V {..})) i =
+  unsafePrimToPrim $ withForeignPtr vptr $ \ptr ->
+    peekElemOff ptr (fromIntegral $! i * vinc)
+
+write :: (PrimMonad m, Storable a) =>
+         Mut (V n) (PrimState m) a -> N n -> a -> m ()
+write v (N i) a = unsafeWrite v i a
 
 unsafeWrite :: (PrimMonad m, Storable a) =>
                Mut (V n) (PrimState m) a -> I -> a -> m ()
-unsafeWrite (Mut (V _ fptr inc)) i a =
-  unsafePrimToPrim $ withForeignPtr fptr $ \ptr ->
-    pokeElemOff ptr (fromIntegral $! i * inc) a
-
-write :: (PrimMonad m, Storable a, i < n) =>
-         Mut (V n) (PrimState m) a -> N i -> a -> m ()
-write v@(Mut (V nn _ _)) ni@(N i) a
-  | lessThan ni nn = unsafeWrite v i a
-  | otherwise =
-      error ("write: index `" ++ show ni
-              ++ "' out of bounds `" ++ show nn ++ "'")
+unsafeWrite (Mut (V {..})) i a =
+  unsafePrimToPrim $ withForeignPtr vptr $ \ptr ->
+    pokeElemOff ptr (fromIntegral $! i * vinc) a
 
 slice :: (Storable a, i < n, i + l <= n) =>
          N i -> N l -> Mut (V n) s a -> Mut (V l) s a
-slice ni@(N i) nl v@(Mut (V nn _ _))
-  | lessThan ni nn && lessThanOrEqual (plus ni nl) nn =
-      unsafeSlice i nl v
-  | otherwise =
-      error ("slice (" ++ show ni ++ ", " ++ show nl
-              ++ ") out of bounds `" ++ show nn ++ "'")
+slice (N i) vdim' (Mut v) =
+  Mut V { vdim = vdim'
+        , vptr = advanceForeignPtr (vptr v) (fromIntegral $! i * vinc v)
+        , vinc = vinc v
+        }
 
 copy :: (PrimMonad m, Storable a) => Mut (V n) (PrimState m) a -> V n a -> m ()
 copy a@(Mut (V (N n) _ _)) b = do
@@ -117,11 +119,6 @@ copy a@(Mut (V (N n) _ _)) b = do
       unsafeIndexPrim b i >>= unsafeWrite a i
       when (i /= 0) (copy_go (i - 1))
   when (n > 0) (copy_go (n - 1))
-
-unsafeSlice :: Storable a => I -> N l -> Mut (V n) s a -> Mut (V l) s a
-unsafeSlice i nl (Mut (V _ fptr inc)) =
-  let fptr' = advanceForeignPtr fptr (fromIntegral $! i * inc) in
-    Mut (V nl fptr' inc)
 
 advanceForeignPtr
   :: forall a. Storable a =>
