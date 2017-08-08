50b20b0be60467fd662542d3c3a75b9f9dd2c760
Thomas Tuegel <ttuegel@mailbox.org>
2017-08-08 08:47:25 -0500

    dblat: Add `dnrm2' tests using known data from BLAS test suite

--- tests/dblat.hs
+++ tests/dblat.hs
@@ -10,8 +10,13 @@ import qualified Hedgehog.Gen as Gen
 import qualified Hedgehog.Range as Range
 
 import Data.Int.Blas
-import Data.Vector.Blas as V
+import Data.Vector.Blas (V, litV)
 import Numeric.LinearAlgebra.Blas
+
+import qualified Data.Vector.Blas as V
+
+dnrm2 :: V s n Double -> ST s Double
+dnrm2 = nrm2
 
 equiv :: Double  -- ^ size
       -> Double  -- ^ test value
@@ -102,6 +107,51 @@ prop_reverse_reverse = property $ do
       V.toList =<< V.reverse =<< V.reverse v
   annotateShow as'
   as === as'
+
+prop_nrm2_known :: Property
+prop_nrm2_known = property $ do
+  let
+    n1s = runST $ do
+      dv1 <- V.slice $(known 0) $(known 0) $(known 1)
+             <$> $(litV [0.1E0, 2.0E0, 2.0E0, 2.0E0, 2.0E0, 2.0E0, 2.0E0, 2.0E0 :: Double])
+      dv2 <- V.slice $(known 0) $(known 1) $(known 1)
+             <$> $(litV [0.3E0, 3.0E0, 3.0E0, 3.0E0, 3.0E0, 3.0E0, 3.0E0, 3.0E0 :: Double])
+      dv3 <- V.slice $(known 0) $(known 2) $(known 1)
+             <$> $(litV [0.3E0, -0.4E0, 4.0E0, 4.0E0, 4.0E0, 4.0E0, 4.0E0, 4.0E0 :: Double])
+      dv4 <- V.slice $(known 0) $(known 3) $(known 1)
+             <$> $(litV [0.2E0, -0.6E0, 0.3E0, 5.0E0, 5.0E0, 5.0E0, 5.0E0, 5.0E0 :: Double])
+      dv5 <- V.slice $(known 0) $(known 4) $(known 1)
+             <$> $(litV [0.1E0, -0.3E0, 0.5E0, -0.1E0, 6.0E0, 6.0E0, 6.0E0, 6.0E0 :: Double])
+      sequence
+        [ dnrm2 dv1
+        , dnrm2 dv2
+        , dnrm2 dv3
+        , dnrm2 dv4
+        , dnrm2 dv5
+        ]
+    n2s = runST $ do
+      dv1 <- V.slice $(known 0) $(known 0) $(known 2)
+             <$> $(litV [0.1E0, 8.0E0, 8.0E0, 8.0E0, 8.0E0, 8.0E0, 8.0E0, 8.0E0 :: Double])
+      dv2 <- V.slice $(known 0) $(known 1) $(known 2)
+             <$> $(litV [0.3E0, 9.0E0, 9.0E0, 9.0E0, 9.0E0, 9.0E0, 9.0E0, 9.0E0 :: Double])
+      dv3 <- V.slice $(known 0) $(known 2) $(known 2)
+             <$> $(litV [0.3E0, 2.0E0, -0.4E0, 2.0E0, 2.0E0, 2.0E0, 2.0E0, 2.0E0 :: Double])
+      dv4 <- V.slice $(known 0) $(known 3) $(known 2)
+             <$> $(litV [0.2E0, 3.0E0, -0.6E0, 5.0E0, 0.3E0, 2.0E0, 2.0E0, 2.0E0 :: Double])
+      dv5 <- V.slice $(known 0) $(known 4) $(known 2)
+             <$> $(litV [0.1E0, 4.0E0, -0.3E0, 6.0E0, -0.5E0, 7.0E0, -0.1E0, 3.0E0 :: Double])
+      sequence
+        [ dnrm2 dv1
+        , dnrm2 dv2
+        , dnrm2 dv3
+        , dnrm2 dv4
+        , dnrm2 dv5
+        ]
+    true = [0.0E0, 0.3E0, 0.5E0, 0.7E0, 0.6E0]
+  annotateShow n1s
+  assert $ and (zipWith (\c t -> equiv t c t) n1s true)
+  annotateShow n2s
+  assert $ and (zipWith (\c t -> equiv t c t) n2s true)
 
 main :: IO ()
 main = void $ checkParallel $$(discover)
